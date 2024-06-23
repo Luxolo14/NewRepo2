de9800b6946e2e1c7afb96ede52bb145770fad52
MrGri <MrGri@LAPTOP-4G8QKO2H>
2024-06-23 15:21:50 +0200

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Core data types of c#
+            String phrase = "Valdora Tempest";
+            char grade = 'A';
+            int age = 26;
+            double Gpa = 3.2;
+            //float,double,decimal
+
+            bool isMale = false;  
+            
+
+            Console.WriteLine(phrase);
+            Console.WriteLine(age);
+            Console.WriteLine(grade);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Container that we can stores values
+            //Assigned and declared
+
+            String characterName = "John";
+
+            int characterAge = 35;
+            Console.WriteLine("There once was a man named " +characterName);
+
+            Console.WriteLine("He was " + characterAge +" years old ");
+            Console.WriteLine("He really liked the name " +characterName);
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
